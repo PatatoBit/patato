95236b71e43219f95a12850ec8d387072fa4750a
PatatoBit <pathonscript@gmail.com>
2026-01-03 15:53:52 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<main class="wrapper">
+	<h1>hi, i'm pat</h1>
+	<p>making things should feel fun</p>
+
+	<ul>
+		<li>email: pathonscript@gmail.com</li>
+		<li><a href="https://github.com/PatatoBit" target="_blank">github</a></li>
+		<li><a href="https://twitter.com/patatobit" target="_blank">twitter</a></li>
+	</ul>
+</main>
